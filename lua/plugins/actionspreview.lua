61bd97e8a1295034a1c1949f4adc436946a6ab02
Burke Strang <Burke.Strang@vertigis.com>
2025-01-18 22:33:20 -0500

    actions preview back

--- lua/plugins/actionspreview.lua
+++ lua/plugins/actionspreview.lua
@@ -1,7 +1,6 @@
-return {}
--- return {
---   "aznhe21/actions-preview.nvim",
---   config = function()
---     vim.keymap.set({ "v", "n" }, "ga", require("actions-preview").code_actions)
---   end,
--- }
+return {
+  "aznhe21/actions-preview.nvim",
+  config = function()
+    vim.keymap.set({ "v", "n" }, "ga", require("actions-preview").code_actions)
+  end,
+}
