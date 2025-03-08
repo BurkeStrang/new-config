6f884b8410213751f300f898deaf8e2e4eed0d51
Burke Strang <burke.strang@outlook.com>
2025-03-07 21:35:09 -0500

    update http

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,1 +1,13 @@
-return { "mistweaverco/kulala.nvim", opts = {} }
+return {
+  "mistweaverco/kulala.nvim",
+  keys = {
+    { "<leader>Rs", desc = "Send request" },
+    { "<leader>Ra", desc = "Send all requests" },
+    { "<leader>Rb", desc = "Open scratchpad" },
+  },
+  ft = { "http", "rest" },
+  opts = {
+    -- your configuration comes here
+    global_keymaps = true,
+  },
+}
