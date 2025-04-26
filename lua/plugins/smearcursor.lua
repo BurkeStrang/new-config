9c0f96c2086d8b0e4afdc5237c9a1ccb9712c865
Burke Strang <Burke.Strang@vertigis.com>
2025-04-26 13:09:53 -0400

    wip

--- lua/plugins/smearcursor.lua
+++ lua/plugins/smearcursor.lua
@@ -1,4 +1,14 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    cursor_color = "#4140A8",
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    distance_stop_animating = 0.5,
+    trailing_exponent = 5,
+    hide_target_hack = true,
+    gama = 1,
+    cterm_cursor_colors = { 240, 245, 250 , 255 },
+    cterm_bg = 235,
+  },
 }
